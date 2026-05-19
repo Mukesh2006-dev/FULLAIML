a2447975144e905975e09c3b0e03d79c4b4c427a
Mukesh2006-dev <alapatimukesh42@gmail.com>
2026-05-19 17:20:36 +0530

    CORS was implemented

--- main.py
+++ main.py
@@ -18,6 +18,19 @@ Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 
 
+origins = [
+    "http://localhost:3000",  # React
+    "http://localhost:5173",  # Vite React
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def home():
     return {"message": "ML Analysis Project running"}
